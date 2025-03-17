f38bb6930c633e5349741eac8dbb119398b97517
asyrofuddien <asyrofuddien11@gmail.com>
2025-03-17 13:41:44 +0700

    remove un used code

--- client/app/lib/apolloClient.tsx
+++ client/app/lib/apolloClient.tsx
@@ -1,16 +1,4 @@
-import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
-import { onError } from '@apollo/client/link/error';
-
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
-    );
-  }
-  if (networkError) {
-    console.log(`[Network error]: ${networkError}`);
-  }
-});
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const createApolloClient = () => {
   return new ApolloClient({
